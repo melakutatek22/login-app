27dbc6e3244605e763037c2fa0e1686717a258e9
melaku8080 <melakutatek22@gmail.com>
2025-05-22 16:29:42 +0300

    added

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: 'db',
   user: 'postgres',
-  password: 'postgres',
+  host: 'localhost',
   database: 'login_db',
+  password: 'postgres',
   port: 5432,
 });
 
